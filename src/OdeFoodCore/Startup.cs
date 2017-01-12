158d08cbf1f67a56cab5b84de761a16ae4fe937d
Sean <seon2you@hotmail.com>
2017-01-12 01:02:05 -0800

    configuration service in startup file

--- src/OdeFoodCore/Startup.cs
+++ src/OdeFoodCore/Startup.cs
@@ -27,6 +27,7 @@ namespace OdeFoodCore
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
